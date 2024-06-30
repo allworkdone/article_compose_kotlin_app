126e73b4478f4a69699656c2dba54ca00f107af2
allworkdone <raj635321@gmail.com>
2024-07-01 00:49:44 +0530

    Task: Performed various task as per given ref : https://developer.android.com/codelabs/basic-android-kotlin-compose-composables-practice-problems?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-1-pathway-3%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-composables-practice-problems#1 Tasks as follow: Set the image to fill the entire screen's width. Set the first Text composable to a 24sp font size and 16dp padding (start, end, bottom, and top). Set the second Text composable to a default font size, 16dp padding(start and end), and Justify text align. Set the third Text composable to a default font size, 16dp padding (start, end, bottom, and top), and Justify text align.

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +27,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeArticleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ContentImage(
+                        first = stringResource(R.string.heading),
+                        second = stringResource(R.string.first_description),
+                        third = stringResource(R.string.second_description),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +39,58 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// First Task
+// Added all the text to the app and the styling
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ContentText(first: String,second: String,third: String, modifier: Modifier = Modifier) {
+    Column{
+        Text(
+            text = first,
+            modifier = modifier.padding(
+                16.dp
+            ),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = second,
+            modifier = modifier.padding(
+                start = 16.dp,
+                end = 16.dp
+            ),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = third,
+            modifier = modifier.padding(16.dp),
+        )
+    }
+}
+
+// Second Task
+// Added the image to the app
+@Composable
+fun ContentImage(first: String,second: String,third: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Column(
+        modifier
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        ContentText(first = first, second = second, third = third, modifier = modifier)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        ContentImage(
+            first = stringResource(R.string.heading),
+            second = stringResource(R.string.first_description),
+            third = stringResource(R.string.second_description)
+        )
     }
 }
